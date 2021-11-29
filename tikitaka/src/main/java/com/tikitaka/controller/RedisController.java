81e71613bf541934b9ebf62331d3233b29d40e14
jung-jinwoo <wlsdn960319@gmail.com>
2021-11-29 18:06:10 +0900

    developer test

---
+++ tikitaka/src/main/java/com/tikitaka/controller/RedisController.java
@@ -0,0 +1,23 @@
+package com.tikitaka.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class RedisController {
+
+	
+
+	
+	
+//	@GetMapping
+//	public String get() {
+//		return urepo.findAll().toString();
+//	}
+//	
+//	@GetMapping
+//	public String set(@RequestBody User user) {
+//		return urepo.save(user).toString();
+//	}
+}
---
+++ tikitaka/src/main/java/com/tikitaka/service/ChatService.java
@@ -0,0 +1,18 @@
+package com.tikitaka.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tikitaka.model.Chat;
+import com.tikitaka.repository.ChatRepository;
+
+@Service
+public class ChatService {
+
+	@Autowired
+	private ChatRepository chatRepository;
+	
+	public void insert(Chat chat) {
+		chatRepository.insert(chat);
+	}
+}
