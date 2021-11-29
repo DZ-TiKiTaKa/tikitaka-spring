8b06cb01f042ce6c1b7a4ee9b7ef0ac7a7fe48ee
naeunkim1227 <kne0140@daum.net>
2021-11-29 18:11:37 +0900

    21.11.29

--- tikitaka/src/main/java/com/tikitaka/repository/UserRedisRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tikitaka.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tikitaka.model.User;
-
-public interface UserRedisRepository extends CrudRepository<User, String> {
-	
-}
