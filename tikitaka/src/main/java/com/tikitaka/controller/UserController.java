529f7cd93b0cf2342a4e56ef9ec31495192c0711
yangsw3114 <72145880+yangsw3114@users.noreply.github.com>
2021-12-06 18:48:59 +0900

    seungwoo / [create] / User MVC

--- tikitaka/src/main/java/com/tikitaka/controller/UserController.java
+++ tikitaka/src/main/java/com/tikitaka/controller/UserController.java
@@ -1,22 +1,30 @@
 package com.tikitaka.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tikitaka.model.User;
+import com.tikitaka.service.UserService;
 
 @RestController
 public class UserController {
 	
-	@GetMapping("/api/hello")
-	public String joinUser(){
-		return "hello ysw";
-	}
-	
-	@RequestMapping("/joinsuccess")
-	public void joinsuccess(@RequestBody User user) {
+	@Autowired
+	private UserService userService;
+		
+	@RequestMapping("/login")
+	public void userlogin(@RequestBody User user) {
 		System.out.println(user.toString());
+		System.out.println("userlogin 메서드 실행");
+		
+		User userr = userService.getUser(user.getEmail(), user.getPassword());
+		
+		
+		System.out.println("name: " + userr.getName());
+		
+		
 	}
 }
