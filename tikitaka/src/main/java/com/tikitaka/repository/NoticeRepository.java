11ffa9ac76d6a95aa88cab79a1c516c70b518bc1
naeunkim1227 <kne0140@daum.net>
2021-12-09 21:08:54 +0900

    naeun / [create] / NoticeController,Repository,Service,mapper 생성

---
+++ tikitaka/src/main/java/com/tikitaka/repository/NoticeRepository.java
@@ -0,0 +1,23 @@
+package com.tikitaka.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.tikitaka.model.Notice;
+
+@Repository
+public class NoticeRepository {
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	public List<Notice> getNotice(String no) {
+ 		
+	return sqlSession.selectList("notice.getNotice", no);	
+	}
+}
