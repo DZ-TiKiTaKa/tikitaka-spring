67ecfc44a71ee3603e2e455e7a3f52e5ce042008
sysyo <youngiya77@gmail.com>
2021-12-10 12:05:32 +0900

    sysyo / [update] / User-logStatus (Controller, Service, Repository, xml 수정)

--- tikitaka/src/main/java/com/tikitaka/service/UserService.java
+++ tikitaka/src/main/java/com/tikitaka/service/UserService.java
@@ -1,5 +1,7 @@
 package com.tikitaka.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,20 +13,22 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public User getUser(String email, String password) {
 		return userRepository.findByIdAndPassword(email, password);
 	}
-	
+
 	public void joinUser(User user) {
 		userRepository.insertUser(user);
 	}
-	
-	public User getLogStatus(Long no) {
-		return userRepository.findLogStatus(no);
 
-	//회원 로그인/로그아웃 상태 업데이트 메소드
-	public boolean UpdateUserState(Long no,int status) {
-		return userRepository.UpdateUserState(no,status);
+	// 친구 목록 (no, role, name, status, profile 가져오기)	
+	public List<User> getLogStatus(String no) {
+		return userRepository.findLogStatus(no);
+	}
+
+	// 회원 로그인/로그아웃 상태 업데이트 메소드
+	public boolean UpdateUserState(Long no, int status) {
+		return userRepository.UpdateUserState(no, status);
 	}
 }
