4433e1a7d55e32a03bbaff323e2755d488e09f34
naeunkim1227 <kne0140@daum.net>
2021-12-15 09:53:44 +0900

    naeun / [update] / socket test

--- tikitaka/src/main/java/com/tikitaka/config/RedisConfig.java
+++ tikitaka/src/main/java/com/tikitaka/config/RedisConfig.java
@@ -24,14 +24,14 @@ import com.tikitaka.redis.RedisMessageSubscriber;
 @PropertySource("application.properties")
 @EnableRedisRepositories //RedisRepository 를 사용한다고 지정
 public class RedisConfig {
-	@Value("${spring.redis.port}")
+   @Value("${spring.redis.port}")
     private int port;
-		
+      
     @Value("${spring.redis.host}")
     private String host;
 
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
+//    @Autowired
+//    private SimpMessagingTemplate simpMessagingTemplate;
     //Redis server와 연결
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
@@ -42,26 +42,26 @@ public class RedisConfig {
     //channel의 메세지를 받는데 사용하는 컨테이너
     @Bean
     public RedisTemplate<String, Object> redisTemplate(){
-    	RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-    	redisTemplate.setConnectionFactory(redisConnectionFactory());
-    	redisTemplate.setKeySerializer(new StringRedisSerializer());
+       RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+       redisTemplate.setConnectionFactory(redisConnectionFactory());
+       redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
-    	return redisTemplate;
+       return redisTemplate;
     }
     
     //긴 문자열을 다룰때 사용
     @Bean
     public StringRedisTemplate strRedisTemplate() {
-    	StringRedisTemplate redisTemplate = new StringRedisTemplate();
-    	redisTemplate.setConnectionFactory(redisConnectionFactory());
-    	return redisTemplate;
+       StringRedisTemplate redisTemplate = new StringRedisTemplate();
+       redisTemplate.setConnectionFactory(redisConnectionFactory());
+       return redisTemplate;
     }
     
     //메시지 큐를 위한 Bean 등록
     //subscriber역할을 수행
     @Bean
     MessageListenerAdapter messageListener() { 
-        return new MessageListenerAdapter(new RedisMessageSubscriber(simpMessagingTemplate));
+        return new MessageListenerAdapter(new RedisMessageSubscriber());
     }
     
     //채널의 메시지를 받는데 사용되는 컨테이너
