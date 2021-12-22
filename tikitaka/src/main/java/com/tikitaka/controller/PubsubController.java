bb6cf9b009943856d9832c647b92c826e4bedc6a
jung-jinwoo <wlsdn960319@gmail.com>
2021-12-23 03:02:02 +0900

    jinwoo / [update] / Calendar

--- tikitaka/src/main/java/com/tikitaka/controller/PubsubController.java
+++ tikitaka/src/main/java/com/tikitaka/controller/PubsubController.java
@@ -1,9 +1,6 @@
 package com.tikitaka.controller;
 
 
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +10,6 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.Topic;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.tikitaka.dto.JsonResult;
 import com.tikitaka.model.Calendar;
+import com.tikitaka.model.CalendarModel;
 import com.tikitaka.model.Chat;
 import com.tikitaka.model.ChatMember;
 import com.tikitaka.model.ChatMessage;
@@ -59,8 +56,6 @@ public class PubsubController {
 	    private RedisSubscriber redisSubscriber;
 	    // topic 이름으로 topic정보를 가져와 메시지를 발송할 수 있도록 Map에 저장
 	    private Map<String, ChannelTopic> channel;
-	    
-	    
 	    
 
 	    ////////////DB Service//////////////////////
@@ -127,7 +122,7 @@ public class PubsubController {
 	        	System.out.println("redis channel 다시 생성하기 init 데이터 전송");
 	        	ChannelTopic topic = new ChannelTopic(chatNo.toString());
 	        	channel.put(chatNo.toString(), topic);
-		    	Messagemodel model = new Messagemodel(userNo,chatNo, "", "연결 되었습니다!", "","","");       
+		    	Messagemodel model = new Messagemodel(userNo,chatNo, "", "", "","","");       
 		        redisMessageListenerContainer.addMessageListener(redisSubscriber, topic);
 		        redisPublisher.publish(topic,model);
 	        }
@@ -298,11 +293,11 @@ public class PubsubController {
 	    }
 	    
 	    @PostMapping("/topic/addCalendar")
-	    public Calendar addCalendar(@RequestBody Calendar cal) {
+	    public void addCalendar(@RequestBody Calendar cal) {
 	    	calendarService.addCalendar(cal);
-	    	System.out.println(cal);
-	    	System.out.println(channel.containsKey("1110"));
-	    	return cal;
+	    	CalendarModel calModel = new CalendarModel(cal.getUserNo(), cal.getTitle(), cal.getContents(), cal.getStartDate(), cal.getEndDate(), cal.getChatNo());
+	    	redisPublisher.publishCal(ChannelTopic.of(cal.getChatNo().toString()),calModel);
+	    	
 	    }
 	    
 	    
