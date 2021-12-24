3dfdfc78752a94940731fff836bd9bb4b270fadf
sysyo <youngiya77@gmail.com>
2021-12-24 13:08:51 +0900

    sysyo / [update] / 연락처 완료!

--- tikitaka/src/main/java/com/tikitaka/service/RedisSubscriber.java
+++ tikitaka/src/main/java/com/tikitaka/service/RedisSubscriber.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tikitaka.model.CalendarModel;
+import com.tikitaka.model.ContactModel;
 import com.tikitaka.model.Messagemodel;
 
 @Service
@@ -46,6 +47,13 @@ public class RedisSubscriber implements MessageListener {
 			String pubCal = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
 			CalendarModel calModel = objectMapper.readValue(pubCal, CalendarModel.class);
 			
+			// 연락처 전송 
+			String pubCon = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
+			ContactModel conModel = objectMapper.readValue(pubCon, ContactModel.class);
+			
+			System.out.println(messageModel.getType() + "df?????????");
+			System.out.println(conModel + "-----------------------------------");
+			
 			// channel 방번호
 			String chatNo = new String(message.getChannel());
 						
@@ -55,12 +63,17 @@ public class RedisSubscriber implements MessageListener {
 					calModel.getEndDate() != null) {
 				System.out.println("Redis Sub calModel send!!");
 				messagingTemplate.convertAndSend("/topic/" + chatNo, calModel);;
+		    
 			} else if(messageModel.getType() != null){
 				// sub한 채널에 데이터 전송
 				messagingTemplate.convertAndSend("/topic/" + chatNo, messageModel);
 				System.out.println("전송 데이터 : " + chatNo);
+			} else if (messageModel.getType() != null && 
+					conModel.getName() != null) {
+				messagingTemplate.convertAndSend("/topic/" + chatNo, conModel);
+				System.out.println("RedisSubcriber-------------------------" + conModel);
 			}
-			
+ 			
 		} catch (Exception e) {
 			System.out.println("error : " + e);
 		}
